d4027cb81911e6e399efcd3024d60ca8ae1b7dd1
Elian Cordoba <eliancordoba98@gmail.com>
2022-04-02 13:08:52 -0300

    Added logic to update driver status and code to calculate driver stats

--- src/routes/driver.ts
+++ src/routes/driver.ts
@@ -1,9 +1,28 @@
-import { differenceInDays, subMonths } from "date-fns";
-import { DeliveryRepository, DriverRepository } from "../services/collections";
+import {
+  differenceInDays,
+  differenceInSeconds,
+  startOfDay,
+  startOfMonth,
+  startOfWeek,
+  subMonths,
+} from "date-fns";
+import {
+  Delivery,
+  DeliveryRepository,
+  Driver,
+  DriverRepository,
+} from "../services/collections";
+import { exec } from "../utils";
 import { RegisterHandlerFn } from "../types";
 
 const deliveryRepository = new DeliveryRepository();
 const driverRepository = new DriverRepository();
+
+interface Stats {
+  activeHours: number;
+  hourlyRate: number;
+  utilizationRate: number;
+}
 
 const driverRoutes: RegisterHandlerFn = (server, _, next) => {
   server.get<{
@@ -21,17 +40,40 @@ const driverRoutes: RegisterHandlerFn = (server, _, next) => {
     }
   });
 
-  server.patch<{ Params: { driverId: string }; Body: { is_active: boolean } }>(
-    "/:driverId",
-    async (req) => {
-      return driverRepository.update(req.params.driverId, {
-        is_active: req.body.is_active,
-      });
+  server.patch<{
+    Params: { driverId: string };
+    Body: { is_active: boolean; timestamp: string };
+  }>("/:driverId", async (req, res) => {
+    const driverId = req.params.driverId;
+    const isActive = req.body.is_active;
+    const timestamp = req.body.timestamp;
+
+    const dateTo = timestamp ? new Date(timestamp) : new Date();
+
+    let update: Partial<Driver> = { is_active: isActive };
+
+    // If the driver from active to inactive, we need to calculate the amount of time they were active and store that result
+    // if it's the other way around we set the `active_at` property to later calculate the above mentioned
+    if (!isActive) {
+      const [driver, error] = await exec(driverRepository.findById(driverId));
+
+      if (error) {
+        res.status(404).send({ error: "Driver not found" });
+      }
+
+      const hoursDone = differenceInSeconds(driver!.active_at, dateTo) / 3600;
+
+      update["active_hours"] = driver!.active_hours + hoursDone;
+    } else {
+      update["active_at"] = dateTo;
     }
-  );
+
+    return driverRepository.update(req.params.driverId, update);
+  });
 
   server.get<{
-    Querystring: { summary?: string };
+    Querystring: { summary: string };
+    Params: { driverId: string };
   }>(
     "/:driverId",
     {
@@ -46,17 +88,86 @@ const driverRoutes: RegisterHandlerFn = (server, _, next) => {
     },
     async (req, res) => {
       const isSummaryRequest = req.query?.summary === "1";
+      const driverId = req.params.driverId;
 
       if (!isSummaryRequest) {
         return res.callNotFound();
       }
 
-      return res.send();
+      const [driver, error] = await exec(driverRepository.findById(driverId));
+
+      if (error) {
+        return res
+          .status(404)
+          .send({ error: `Driver ${driverId} was not found ` });
+      }
+
+      const deliveries = await deliveryRepository.getAllFromDriver(
+        req.params.driverId
+      );
+
+      const [daysActive, countDayFn] = calculateStats(startOfDay);
+      const [weeksActive, countWeeksFn] = calculateStats(startOfWeek);
+      const [monthsActive, countMonth] = calculateStats(startOfMonth);
+
+      for (const delivery of deliveries) {
+        countDayFn(delivery);
+        countWeeksFn(delivery);
+        countMonth(delivery);
+      }
+
+      const activeHours = driver?.active_hours as number;
+
+      return {
+        daysActive: addStatsResults(activeHours, daysActive),
+        weeksActive: addStatsResults(activeHours, weeksActive),
+        monthsActive: addStatsResults(activeHours, monthsActive),
+      };
     }
   );
 
   next();
 };
+
+function addStatsResults(
+  activeHours: number,
+  partialStats: Map<string, { utilizationHours: number; earned: number }>
+) {
+  const result: Record<string, Stats> = {};
+
+  for (const [date, information] of partialStats.entries()) {
+    result[date] = {
+      activeHours,
+      hourlyRate: information.earned / activeHours,
+      utilizationRate: (activeHours * 100) / activeHours,
+    };
+  }
+
+  return result;
+}
+
+function calculateStats(dateFn: (date: Date) => Date) {
+  const result: Map<string, { utilizationHours: number; earned: number }> =
+    new Map();
+
+  return [
+    result,
+    (delivery: Delivery) => {
+      const key = dateFn(delivery.created_at!).toISOString();
+      const item = result.get(key) || { utilizationHours: 0, earned: 0 };
+
+      const utilizationHours =
+        Math.abs(
+          differenceInSeconds(delivery.pickup_time, delivery.dropoff_time)
+        ) / 3600;
+
+      result.set(key, {
+        utilizationHours: item.utilizationHours + utilizationHours,
+        earned: item.earned + delivery.order_subtotal,
+      });
+    },
+  ] as const;
+}
 
 async function handleGetDriverSummary(driverId: string) {
   const dateTo = new Date();
